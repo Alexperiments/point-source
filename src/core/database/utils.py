5eb9a6854e29690f7bd46d3d5feca65b42593fea
Alessandro Diana <dianaalessandro96@gmail.com>
2026-01-23 02:39:28 +0100

    feat: create primitive database insert many function

---
+++ src/core/database/utils.py
@@ -0,0 +1,26 @@
+"""Database primitive operations."""
+
+from collections.abc import Sequence
+
+from sqlalchemy import literal
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from src.core.database.base import DeclarativeMetaBase
+
+
+async def insert_many(
+    session: AsyncSession,
+    table: type[DeclarativeMetaBase],
+    values: Sequence[dict],
+) -> int:
+    """Insert many in the table referenced by the provided SQLAlchemy table object.
+
+    Return the number of inserted rows.
+    """
+    result = await session.execute(
+        insert(table).on_conflict_do_nothing().returning(literal(1)),
+        values,
+    )
+    await session.flush()
+    return sum(1 for _ in result)
